257d42818684c3c3b5d26a82196c23578b066a36
stoyj <stoyanka.kamenova@gmail.com>
2025-01-27 17:35:57 +0200

    working with array Methods

--- task4.js
+++ task4.js
@@ -24,4 +24,28 @@
 //   { title: "Song D", duration: 2.8 }
 // ]
 
-// Дай ми да разбера кода и да реша стъпка по стъпка и по-прост да е кода
+function addTransitions(playlist) {
+  for (let i = 0; i < playlist.length - 1; i++) {
+    playlist[i].title += "- Transition";
+  }
+  return playlist; // Връщаме обновения плейлист
+}
+
+// Примерен вход:
+const playlist = [
+  { title: "Song A", duration: 3.5 },
+  { title: "Song B", duration: 4.2 },
+  { title: "Song C", duration: 5.1 },
+  { title: "Song D", duration: 2.8 },
+];
+
+// Извикваме функцията:
+const updatedPlaylist = addTransitions(playlist);
+console.log(updatedPlaylist);
+
+// [
+// { title: "Song A - Transition", duration: 3.5 },
+// { title: "Song B - Transition", duration: 4.2 },
+// { title: "Song C - Transition", duration: 5.1 },
+// { title: "Song D", duration: 2.8 }
+// ]
